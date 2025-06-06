394d38dc1a91e185a340db8c51f55b4e75b5b236
Taticek <TaticekTo@vda.cz>
2025-06-06 09:11:43 +0200

    pridani operaci

--- kalkulator.cmd
+++ kalkulator.cmd
@@ -8,8 +8,21 @@ set /p operace=napiste operaci /,*,+:
 
 
 
+if %operace%==*: GOTO:SOUCIN
+if %operace%==/: GOTO:DELENI
+if %operace%==+: GOTO:SOUCET
 
 
+
+:SOUCIN
+set /a vysledek=%prvnic% * %druhec%
+echo %prvnic%*%druhec%=%vysledek%
+:DELENI
+set /a vysledek=%prvnic% / %druhec%
+echo %prvnic%/%druhec%=%vysledek%
+:SOUCET
+set /a vysledek=%prvnic% + %druhec%
+echo %prvnic%+%druhec%=%vysledek%
 
 
 
